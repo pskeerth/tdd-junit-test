3a9b63eb79e06d8119fcf4728268dbe13be37306
Poornima Sathya Keerthi <poornimasathyakeerthi@Poornimas-MacBook-Air.local>
2022-10-27 12:25:05 -0700

    intial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Urinals urinals = new Urinals();
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/org/example/Urinals.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import com.sun.deploy.trace.Trace;
+
+import java.util.Scanner;
+
+public class Urinals {
+    Scanner sc = new Scanner(System.in);
+
+    public boolean numberOfPeopleInUrinal(String string) {
+
+        System.out.println("");
+        return true;
+    }
+
+}
