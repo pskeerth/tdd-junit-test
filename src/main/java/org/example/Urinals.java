cf7a862eb3ec1f52b0c4f839118e353425f29829
Poornima Sathya Keerthi <poornimasathyakeerthi@poornimas-air.mynetworksettings.com>
2022-10-27 21:29:03 -0700

    file read write functions

--- src/main/java/org/example/Urinals.java
+++ src/main/java/org/example/Urinals.java
@@ -1,21 +1,46 @@
 package org.example;
 
 
+import java.io.*;
+
 /**
  * AUTHOR: Poornima Sathya Keerthi
  */
 public class Urinals {
-    public static void main(String[] args) {
+    static BufferedWriter bufferedWriter;
+
+    static {
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/output.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         readFromFile();
+        bufferedWriter.close();
     }
-    public static void readFromFile(){
-        System.out.println ("Not yet implemented");
+    public static void readFromFile() throws IOException {
+        String line;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+        while ((line = bufferedReader.readLine()) != null) {
+            writeToFile(countUrinals(line));
+        }
+
     }
-    public static void writeToFile(String string){
-        System.out.println ("Not yet implemented");
+
+    public static void writeToFile(String string) throws IOException {
+        bufferedWriter.write(string);
+        bufferedWriter.newLine();
     }
+
     public static String countUrinals(String string) {
-        System.out.println ("Not yet implemented");
-        return "";}
+        StringBuilder urinal = new StringBuilder(string);
+        int i =0;
+        int count = 0;
+        System.out.println("NOT YET IMPLEMENTED");
+        return String.valueOf(count);
+    }
 
 }
---
+++ src/test/java/org/example/UrinalsTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UrinalsTest {
+
+    @Test
+    void countUrinals() {
+        System.out.println("NOT YET IMPLEMENTED");
+    }
+}
