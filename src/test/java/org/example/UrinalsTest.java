85d380638e90f8a5d4f8984ae768045acec29101
Poornima Sathya Keerthi <poornimasathyakeerthi@poornimas-air.mynetworksettings.com>
2022-10-27 20:43:56 -0700

    implemented logic

---
+++ src/test/java/org/example/UrinalsTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrinalsTest {
+
+
+
+    /**
+     * Tests the success scenario
+     * @throws IOException
+     */
+    @Test
+    void numberOfPeopleInUrinal() {
+        System.out.println("Not yet implemented");
+    }
+
+    /**
+     *
+     * @throws IOException
+     */
+    @Test
+    void stringTooLong() {
+        System.out.println("Not yet implemented");
+    }
+
+    @Test
+    void invalidString() {
+        System.out.println("Not yet implemented");
+    }
+}
