da46808c76f12a052647868b5ab2a7dab37baf30
Poornima Sathya Keerthi <poornimasathyakeerthi@poornimas-air.mynetworksettings.com>
2022-10-27 21:24:27 -0700

    added skeleton functions

---
+++ src/main/java/org/example/Urinals.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+/**
+ * AUTHOR: Poornima Sathya Keerthi
+ */
+public class Urinals {
+    public static void main(String[] args) {
+        readFromFile();
+    }
+    public static void readFromFile(){
+        System.out.println ("Not yet implemented");
+    }
+    public static void writeToFile(String string){
+        System.out.println ("Not yet implemented");
+    }
+    public static String countUrinals(String string) {
+        System.out.println ("Not yet implemented");
+        return "";}
+
+}
