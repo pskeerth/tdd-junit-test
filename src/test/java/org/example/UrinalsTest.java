3f97c6192c51de425398a22d1b8333914beafd46
Poornima Sathya Keerthi <poornimasathyakeerthi@poornimas-air.mynetworksettings.com>
2022-10-27 21:39:35 -0700

    tests

--- src/test/java/org/example/UrinalsTest.java
+++ src/test/java/org/example/UrinalsTest.java
@@ -2,13 +2,31 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.*;
 class UrinalsTest {
 
     @Test
-    void countUrinals() {
-        System.out.println("NOT YET IMPLEMENTED");
+    void numberOfPeopleInUrinal() {
+        System.out.println("Not yet implemented");
+        String expectedOutput = "1";
+        String inputString = "10001";
+        assertEquals(expectedOutput, Urinals.countUrinals(inputString));
     }
+
+    @Test
+    void stringTooLong() {
+        System.out.println("Not yet implemented");
+        String stringTooLongErrorMsg = "String is too long";
+        String inputString = "100000000000000001100";
+        assertEquals(stringTooLongErrorMsg, Urinals.countUrinals(inputString));
+    }
+
+    @Test
+    void invalidString() {
+        System.out.println("Not yet implemented");
+        String stringInvalidErrorMsg = "Invalid input";
+        String inputString = "abc";
+        assertEquals(stringInvalidErrorMsg, Urinals.countUrinals(inputString));
+    }
+
 }
